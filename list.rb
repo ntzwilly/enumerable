680617237278bfddc01b0d6eb51e9c08a9d98d02
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-09 15:32:34 +0100

    changing each method + deleting .github copy folder

--- list.rb
+++ list.rb
@@ -9,11 +9,14 @@ class MyList
   end
 
   def each
+    return to_enum(:each) unless block_given?
+
     i = 0
     while i < @list.length
       yield @list[i]
       i += 1
     end
+    @list
   end
 end
 
