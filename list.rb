2227fc6ed4d149ea916fdc1ba4d7bea9d8ebe30b
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-09 14:14:09 +0100

    writing filter method for list enumerable + fixing rubocop offenses

--- list.rb
+++ list.rb
@@ -1,23 +1,26 @@
 require './enumerable'
 
 class MyList
-    include MyEnumerable
+  include MyEnumerable
 
-    def initialize(*list)
-        @list = list
-    end
+  def initialize(*list)
+    @list = list
+  end
 
-    def each 
-        i = 0
-        while i < @list.length
-            yield @list[i]
-            i += 1
-        end
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
     end
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts list.all? { |e| e < 4 }
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+p list.filter(&:even?)
